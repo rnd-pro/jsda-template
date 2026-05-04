4aa1d7083e0cf64510566230c66e771f1ea4d8a6
foxeyes <foxrandom.com@gmail.com>
2026-05-04 13:29:48 -0300

    chore: update HEADER_CONTENT to remove "Project" suffix from static page titles

--- src/static-pages/pages/cit/index.html.js
+++ src/static-pages/pages/cit/index.html.js
@@ -3,6 +3,6 @@ import { getPage } from '../../getPage.js';
 export default await getPage({
   BASE_PATH: '../../',
   TITLE: 'Cloud Images Toolkit',
-  HEADER_CONTENT: 'JSDAStack Reference Project: Cloud Images Toolkit',
+  HEADER_CONTENT: 'JSDAStack Reference: Cloud Images Toolkit',
   MD_URL: 'https://raw.githubusercontent.com/rnd-pro/cloud-images-toolkit/refs/heads/main/README.md',
 });
--- src/static-pages/pages/template/index.html.js
+++ src/static-pages/pages/template/index.html.js
@@ -3,6 +3,6 @@ import { getPage } from '../../getPage.js';
 export default await getPage({
   BASE_PATH: '../../',
   TITLE: 'Template Repo',
-  HEADER_CONTENT: 'JSDAStack Reference Project: Template Repo',
+  HEADER_CONTENT: 'JSDAStack Reference: Template Repo',
   MD_URL: './README.md',
 });
