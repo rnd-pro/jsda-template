22e8e306e165232fd2cf2a793b3fce36971edcc1
foxeyes <foxrandom.com@gmail.com>
2026-05-07 12:29:08 -0300

    chore: update jsda-kit to v1.5.2 and implement explicit SSR imports for server-only component

--- src/dynamic-pages/no-route-example/index.html.js
+++ src/dynamic-pages/no-route-example/index.html.js
@@ -2,6 +2,11 @@ import fs from 'node:fs';
 import pageData from './pageData.js';
 import { applyData } from 'jsda-kit/iso/applyData.js';
 
+// Endpoint specific dependency to render server component:
+export const ssrImports = [
+  './src/ui-components/server-only/server-info/logic.js',
+];
+
 let template = fs.readFileSync(new URL('./template.html', import.meta.url)).toString();
 
 export default applyData(template, pageData);
--- src/ui-components/ssr-exports.js
+++ src/ui-components/ssr-exports.js
@@ -2,5 +2,5 @@
 export * from './universal/login-widget/logic.js';
 export * from './universal/side-panel/logic.js';
 
-// Server-only components
-export * from './server-only/server-info/logic.js';
+// The code re-exported here will be used on SSR stage only, 
+// so it will not appear in the client-side bundle.
