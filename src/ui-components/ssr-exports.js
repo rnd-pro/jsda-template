b5f80efe5e5acfd63e50419789ace53afa048c33
foxeyes <foxrandom.com@gmail.com>
2026-05-07 12:33:56 -0300

    refactor: update ssr-exports comment and reorder exports for clarity

--- src/ui-components/ssr-exports.js
+++ src/ui-components/ssr-exports.js
@@ -1,6 +1,3 @@
-// Universal isomorphic components
+// Universal isomorphic components for SSR stage only:
 export * from './universal/login-widget/logic.js';
 export * from './universal/side-panel/logic.js';
-
-// The code re-exported here will be used on SSR stage only, 
-// so it will not appear in the client-side bundle.
